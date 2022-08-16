932d6d1694be65565b4815f31e8eff949370ecd8
galya-cmd <galya1985@bk.ru>
2022-08-17 00:21:16 +0300

    создала навигацию и фон

---
+++ barcode/src/Nav.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function Nav (){
+return(
+    <>
+    <ul className="nav">
+<li><a href="/">Главная</a></li>
+<li><a href="/ink">Чернила</a></li>
+<li><a href="/service">Сервис</a></li>
+<li><a href="/guarantees">Гарантии</a></li>
+<li><a href="/production">Производство</a></li>
+<li><a href="/contacts">Контакты</a></li>
+</ul>
+    </>
+)
+}
+
+export default Nav
